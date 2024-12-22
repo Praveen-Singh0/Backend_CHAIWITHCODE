20a6e5d3b3cffb7527bc70788fd804d8d304054b
Praveen-Singh0 <parnbartwal@gmail.com>
2024-12-22 15:27:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const port = 3000;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${port}`);
+});
+
